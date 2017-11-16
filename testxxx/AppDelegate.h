ec3168ffd39338118161d10323ec21fc3b0596b9
steven <464139951@qq.com>
2017-11-16 13:46:13 +0800

    xxx

---
+++ testxxx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testxxx
+//
+//  Created by cinlan on 2017/11/16.
+//  Copyright © 2017年 Cilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testxxx/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testxxx
+//
+//  Created by cinlan on 2017/11/16.
+//  Copyright © 2017年 Cilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
